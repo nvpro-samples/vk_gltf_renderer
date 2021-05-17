5de1b07e7bf74e481baedc07f0ab932c6cfe4531
Mathias Heyer <mheyer@nvidia.com>
2021-05-17 13:10:30 -0700

    Bulk update nvpro-samples 05/17/21
    Changing license from BSD-3 to Apache2

--- vkalloc.hpp
+++ vkalloc.hpp
@@ -1,21 +1,41 @@
 /*
-* Copyright (c) 2014-2020, NVIDIA CORPORATION.  All rights reserved.
-*
-* NVIDIA CORPORATION and its licensors retain all intellectual property
-* and proprietary rights in and to this software, related documentation
-* and any modifications thereto.  Any use, reproduction, disclosure or
-* distribution of this software and related documentation without an express
-* license agreement from NVIDIA CORPORATION is strictly prohibited.
-*/
+ * Copyright (c) 2019-2021, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-FileCopyrightText: Copyright (c) 2019-2021 NVIDIA CORPORATION
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #pragma once
 
 #include <vulkan/vulkan.hpp>
 
-#define NVVK_ALLOC_DEDICATED
-//#define NVVK_ALLOC_DMA
-//#define NVVK_ALLOC_VMA
 
-#include <nvvk/allocator_vk.hpp>
+// Choosing the allocator to use
+#define ALLOC_DMA
+//#define ALLOC_DEDICATED
+//#define ALLOC_VMA
+#include <nvvk/resourceallocator_vk.hpp>
+
+#if defined(ALLOC_DMA)
+#include <nvvk/memallocator_dma_vk.hpp>
+typedef nvvk::ResourceAllocatorDma Allocator;
+#elif defined(ALLOC_VMA)
+#include <nvvk/memallocator_vma_vk.hpp>
+typedef nvvk::ResourceAllocatorVma Allocator;
+#else
+typedef nvvk::ResourceAllocatorDedicated Allocator;
+#endif
 
 // Common structures
 using vkDS = vk::DescriptorSetLayoutBinding;
